86555837e134ae34ba3e3c406ebaf60d75859249
iamcute <2570394541@qq.com>
2017-10-30 17:00:58 +0800

    web-homework js

--- web-homework/moreAct.js
+++ web-homework/moreAct.js
@@ -1,11 +1,15 @@
 var index = 1;
 var timer;
+
 window.onload = function () {
     carousel();
+
 };
 
 function carousel() {
     var container = document.getElementsByClassName("imgContainer")[0];
+
+    autoPlay();
 
     container.onmouseout = autoPlay;
     container.onmousemove = stopPlay;
@@ -15,66 +19,65 @@ function carousel() {
 }
 
 function autoPlay() {
-   timer = setInterval(function () {
-       getIndex();
-       changeImg();
-       changeIndex();
-   },1000);
+
+    timer = setInterval(function () {
+        nextMove();
+    }, 1000);
 }
 
 function stopPlay() {
     clearInterval(timer);
 }
 
-function changeImg() {
+function changeImg(index) {
     var img = document.getElementsByClassName("imgList")[0].getElementsByTagName("img");
-    var len =img.length;
+    var len = img.length;
 
-    for (var i=0;i<len;i++){
+    for (var i = 0; i < len; i++) {
         img[i].className = "";
     }
 
-    img[index-1].className ="img-on";
+    img[index - 1].className = "img-on";
 }
 
-function changeIndex() {
-    var indexList= document.getElementsByClassName("spot")[0].getElementsByTagName("span");
+function changeIndex(index) {
+    var indexList = document.getElementsByClassName("spot")[0].getElementsByTagName("span");
     var len = indexList.length;
 
-    for(var i=0;i<len;i++){
+    for (var i = 0; i < len; i++) {
         indexList[i].className = "";
     }
 
-    indexList[index-1].className="on";
+    indexList[index - 1].className = "on";
 
 }
-function getIndex() {
+
+function nextMove() {
 
     index += 1;
 
-    if (index>4){
+    if (index > 4) {
         index = 1;
     }
-    if (index<1){
+    if (index < 1) {
         index = 4;
     }
+
+    changeIndex(index);
+    changeImg(index);
 }
 
 function splotClick() {
-    var indexList= document.getElementsByClassName("spot")[0].getElementsByTagName("span");
+    var indexList = document.getElementsByClassName("spot")[0].getElementsByTagName("span");
     var len = indexList.length;
 
-    for(var i=0;i<len;i++){
-        indexList[i].className = "";
-    }
-
-    for (var i=0;i<len;i++){
+    for (var i = 0; i < len; i++) {
         (function (i) {
-          indexList[i].onclick=function () {
-              index = indexList[i].getAttribute("index");
-              console.log(index);
-              indexList[index].className = "on";
-          }
-      })(i)
+            indexList[i].onclick = function () {
+                index = indexList[i].getAttribute("index");
+                changeIndex(index);
+                changeImg(index);
+            }
+        })(i)
     }
 }
