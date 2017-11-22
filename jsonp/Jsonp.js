d407418e96f595e0448d07e3c729abaa247f2a7e
iamcute <2570394541@qq.com>
2017-11-22 21:41:56 +0800

    jsonp

--- jsonp/Jsonp.js
+++ jsonp/Jsonp.js
@@ -5,5 +5,4 @@ function jsonp(json) {
     script.setAttribute('src', url + "?appkey=" + json.appkey + "&zipcode=" + json.zipcode + "&callback=" + json.callback);
     document.body.appendChild(script);
 
-    json.success(data);
 }
